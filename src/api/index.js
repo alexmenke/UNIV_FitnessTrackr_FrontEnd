567868f9c222a5e9565b82bf28b081e2fa027a1b
alexmenke <alexrm2017@gmail.com>
2022-10-20 20:57:44 -0500

    added getUserInfo in api

--- src/api/index.js
+++ src/api/index.js
@@ -21,3 +21,19 @@ export const loginUser = async (username, password) => {
         console.log('Error logging in user')
     }
 }
+
+export const getUserInfo = async (token) => {
+    try {
+        const response = await fetch(`${baseURL}/users/me`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+              },
+        })
+
+        const results = await response.json();
+        return results;
+    } catch (ex) {
+        console.log('Error getting user information')
+    }
+}
