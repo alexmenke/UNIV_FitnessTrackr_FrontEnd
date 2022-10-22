4e57cc466ecb5c517d52ad06f545e2988cda4a09
ajordan8970807 <adri.brown89@gmail.com>
2022-10-22 08:52:44 -0500

    another update

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,3 +1,4 @@
+//This route is used to create a new user account. On success, you will be given a JSON Web Token to be passed to the server for requests requiring authentication.
 fetch('http://fitnesstrac-kr.herokuapp.com/api/users/login', {
   method: "POST",
   headers: {
